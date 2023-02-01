2e8507089cb10d1cf8d1fbf873b05a522b3241e8
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2023-02-01 17:09:10 -0600

    adding hook useUser

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,11 +2,10 @@ import { View, StyleSheet, ScrollView, Alert } from 'react-native';
 import TextCustom from './Text';
 import Constants from 'expo-constants';
 import theme from '../theme';
-import { Link, useNavigate } from 'react-router-native';
+import { Link } from 'react-router-native';
 import { useQuery } from '@apollo/client';
 import { GET_ME } from '../graphql/queries';
-import { useApolloClient } from '@apollo/client';
-import useAuthStorage from '../hooks/useAuthStorage';
+import useUser from '../hooks/useUser';
 
 const styles = StyleSheet.create({
     container: {
@@ -27,9 +26,7 @@ const styles = StyleSheet.create({
 const AppBar = () => {
     const { data } = useQuery(GET_ME, { fetchPolicy: 'cache-and-network' });
     const user = data ? data.me : undefined;
-    const authStorage = useAuthStorage();
-    const apolloClient = useApolloClient();
-    const navigate = useNavigate()
+    const {singOutUser} = useUser()
 
     const singOut = () => {
         Alert.alert('', 'Do you want to sing out?', [
@@ -41,9 +38,7 @@ const AppBar = () => {
                 text: 'Sing out',
                 style: 'default',
                 onPress: async () => {
-                    await authStorage.removeAccessToken();
-                    await apolloClient.resetStore();
-                    navigate('/singin');
+                    await singOutUser('/singin')
                 },
             },
         ])
